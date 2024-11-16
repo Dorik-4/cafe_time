f93f5ff37aa9f62663ba703562fb5c943d5a317a
Dorik-4 <d.balyakina.044@gmail.com>
2024-11-16 10:28:15 +0300

    API Products

--- lib/services/menu.dart
+++ lib/services/menu.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:cafe_time/widgets/product_card.dart';
 import 'package:dio/dio.dart';
 
 Future<List<String>> fetchCategories() async {
@@ -19,6 +22,28 @@ Future<List<String>> fetchCategories() async {
       );
 
       return categories;
+    } else {
+      throw Exception('Ошибка при получении данных');
+    }
+  } catch (e) {
+    throw Exception('Не удалось подключиться к API: $e');
+  }
+}
+
+
+Future<List<ProductItem>> fetchMenu() async {
+  const String apiUrl = 'https://hackers54.ru:4433/api/v1.0/static_data/menu';
+  final Dio dio = Dio();
+
+  try {
+    final response = await dio.get(apiUrl);
+
+    if (response.statusCode == 200) {
+      final data = response.data;
+
+      List<dynamic> productsJson = data["products"];
+      return productsJson.map((json) => ProductItem.fromJson(json)).toList();
+
     } else {
       throw Exception('Ошибка при получении данных');
     }
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,15 +1,54 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class ProductCard extends StatelessWidget {
-  final String imageUrl;
-  final String name;
-  final double price;
 
-  ProductCard({
-    required this.imageUrl,
-    required this.name,
+class ProductItem {
+  final int id;
+  final String title;
+  final String description;
+  final String extraInfo;
+  final String photoUrl;
+  final int price;
+  final int idCategory;
+  final bool special;
+
+
+  ProductItem({
+    required this.id,
+    required this.title,
     required this.price,
+    required this.idCategory,
+    this.description = "",
+    this.extraInfo = "",
+    required this.photoUrl,
+    this.special = false,
+
   });
+
+  factory ProductItem.fromJson(Map<String, dynamic> json){
+    return ProductItem(
+        id: json['id'],
+        title: json['title'] ,
+        price: json['price'] ,
+        idCategory: json['id_category'],
+        photoUrl: json ['photo_url'],
+        description: json['description'],
+        extraInfo: json['extra_info'],
+        special: json['isSpecial'],
+    )
+  }
+
+}
+
+
+class ProductCard extends StatelessWidget {
+  final ProductItem item;
+
+  const ProductCard({
+   super.key,
+   required this.item,
+});
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +56,20 @@ class ProductCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(imageUrl, height: 200, fit: BoxFit.cover),
+          Image.network(item.photoUrl, height: 200, fit: BoxFit.cover),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  name,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  item.title,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 4),
+                const SizedBox(height: 4),
                 Text(
-                  'Цена: $price руб.',
-                  style: TextStyle(fontSize: 16, color: Colors.green),
+                  '${'Цена: ${item.price}'} руб.',
+                  style: const TextStyle(fontSize: 16, color: Colors.green),
                 ),
               ],
             ),
